711fef5acf4f8de12e8016bc1a583e3dbd1a5c3a
Natalya Ryzhikh <natalya.ryzhikh@umbrellait.com>
2024-03-03 16:02:02 +0300

    Решение задачи о сравнении строк

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,22 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  return [...arr].sort(param === 'asc' ? compareAsc : compareDesc);
+}
 
+function compareAsc(str1, str2) {
+  return compareLinguistic(str1, str2);
+}
+
+function compareDesc(str1, str2) {
+  return compareLinguistic(str2, str1);
+}
+
+function compareLinguistic(str1, str2) {
+  if (str1[0] !== str2[0]
+    && str1[0].toLowerCase() === str2[0].toLowerCase()) {
+    return str1[0].toLowerCase() === str1[0] ? 1 : -1;
+  }
+
+  return str1.localeCompare(str2);
 }
