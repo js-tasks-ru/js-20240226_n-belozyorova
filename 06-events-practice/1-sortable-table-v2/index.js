d7cc843f69a8de32fd69663ee03ec97afe592812
Natalya Ryzhikh <natalya.ryzhikh@umbrellait.com>
2024-03-18 00:01:30 +0300

    Решение задач 1, 2 из 6-го  модуля

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,65 @@
-export default class SortableTable {
+
+import SortableTableV1 from '../../05-dom-document-loading/2-sortable-table-v1';
+
+export default class SortableTable extends SortableTableV1 {
   constructor(headersConfig, {
     data = [],
     sorted = {}
   } = {}) {
+    super(headersConfig, data);
+    this.sorted = sorted;
 
+    this.makeSubelementsForTests();
+    this.initialSort();
+    this.createEventListeners();
+  }
+
+  makeSubelementsForTests() {
+    this.subElements.header = {};
+    this.subElements.header.children = Object.values(this.subElements.headerCells);
+  }
+
+  initialSort() {
+    if (!this.sorted?.id || !this.sorted?.order) {
+      return;
+    }
+
+    this.sort(this.sorted?.id, this.sorted?.order);
+  }
+
+  handleHeaderCellClick = (e) => {
+    const cellElement = e.target.closest('[data-sortable="true"]');
+
+    if (!cellElement) {
+      return;
+    }
+
+    const dataset = cellElement.dataset;
+    this.sort(dataset.id, dataset.order === 'desc' ? 'asc' : 'desc');
+  }
+
+  createEventListeners() {
+    for (let cell of Object.values(this.subElements.headerCells)) {
+      if (!cell.dataset.sortable) {
+        continue;
+      }
+
+      cell.addEventListener('pointerdown', this.handleHeaderCellClick);
+    }
+  }
+
+  destroyEventListeners() {
+    for (let cell of Object.values(this.subElements.headerCells)) {
+      if (!cell.dataset.sortable) {
+        continue;
+      }
+
+      cell.removeEventListener('pointerdown', this.handleHeaderCellClick);
+    }
+  }
+
+  destroy() {
+    this.destroyEventListeners();
+    super.destroy();
   }
 }
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,72 @@
 class Tooltip {
-  initialize () {
+  static instance;
+  dataSelector = 'tooltip';
 
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
+    this.element = this.createElement(this.createTemplate);
+  }
+
+  initialize () {
+    this.createEventListeners();
+  }
+
+  createTemplate() {
+    return `<div class="tooltip"></div>`;
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  render(targetContainer) {
+    if (!targetContainer) {
+      targetContainer = document.body;
+    }
+
+    targetContainer.appendChild(this.element);
+  }
+
+  createEventListeners() {
+    document.addEventListener('pointerover', this.onDocumentPointerOver);
+    document.addEventListener('pointerout', this.onDocumentPointerOut);
+  }
+
+  destroyEventListeners() {
+    document.removeEventListener('pointerover', this.onDocumentPointerOver);
+    document.removeEventListener('pointerout', this.onDocumentPointerOut);
+  }
+
+  setText(text) {
+    this.element.innerHTML = text;
+  }
+
+  onDocumentPointerOver = (e) => {
+    const tooltipText = e.target?.dataset[this.dataSelector];
+
+    if (tooltipText) {
+      this.setText(tooltipText);
+      this.render();
+    }
+  };
+
+  onDocumentPointerOut = (e) => {
+    this.remove();
+  };
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.destroyEventListeners();
+    this.remove();
   }
 }
 
